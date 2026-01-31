f980e76880c1247ec3c4d468189afa606afe4ac3
cqvers <cqvers@gmail.com>
2026-01-31 16:19:01 -0500

    basic interval update for title embedded into browser

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-let titles = ['blurrys', 'bio']
+let titles = ['kalebs', 'bio']
 let index = 0
 let interval = setInterval(() => {
     document.title = titles[index]
     index += 1
     if (index === titles.length) index = 0
-}, 500)
+}, 800)
